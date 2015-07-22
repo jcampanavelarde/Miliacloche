c3a95cf21000377df96693dece7f49e995472a3e
Javier Campana <jcampana@cyg.ec>
2015-07-22 01:09:53 -0500

    [ADD] Milia_Cloche

---
+++ control_milia/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014-Today
+#    CyG xxxx
+#    @autor: Javier Campana <jcampana@cyg.ec>
+#
+##############################################################################
+{
+    "name": "Milia Cloche",
+    "version": "(22.07.2015)",
+    "author": "CyG",
+    "website": "http://www.cyg.ec",
+    "category": "Zapatos",
+    "sequence": 1,
+    "complexity": "expert",
+    "description": """
+    Modulo Local Zapatos
+    """,
+    "shortdesc": "Milia Cloche",
+    "depends": [],
+    "init_xml": [],
+    "demo_xml": [],
+    "update_xml": [],
+    "application": True,
+    "auto_install": False,
+}
+
